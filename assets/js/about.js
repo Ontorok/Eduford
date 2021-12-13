429b40974ea7682ffe55dd3b8ca39d97cb102306
Ontorok <nasir.amath07@gmail.com>
2021-12-13 17:18:13 +0600

    course page half done

--- assets/js/about.js
+++
@@ -1,9 +0,0 @@
-const navLinks = document.querySelector('.sub-header .nav-links')
-
-function showMenu() {
-    navLinks.style.right = '0px'
-}
-
-function hideMenu() {
-    navLinks.style.right = '-200px'
-}
