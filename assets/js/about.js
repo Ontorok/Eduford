188cff98a1f9c67e06d1705e62cb1d71549cec52
Ontorok <nasir.amath07@gmail.com>
2021-12-13 16:14:53 +0600

    add  about page

---
+++ assets/js/about.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelector('.sub-header .nav-links')
+
+function showMenu() {
+    navLinks.style.right = '0px'
+}
+
+function hideMenu() {
+    navLinks.style.right = '-200px'
+}
